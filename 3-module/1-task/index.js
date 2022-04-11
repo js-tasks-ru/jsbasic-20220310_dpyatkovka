f6d86004cd658b32c6852c09c493e045ad598aa4
Dima Pyatkovka <dpyatkovka@gmail.com>
2022-04-11 17:34:33 +0200

    3-module, task 1-5

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,13 @@
 function namify(users) {
-  // ваш код...
+  let result = [];
+  
+  for (let userObj of users ){
+    for (let key in userObj){
+      if (key === 'name'){
+        result.push(userObj[key]);
+      }
+    }
+  }
+  
+  return result;
 }
